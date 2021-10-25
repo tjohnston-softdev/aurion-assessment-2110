6f8553a85ffc1a791a5ac30d2bad8d2719c4173d
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-10-25 15:09:39 +1000

    Wrote 'ignore' unit tests for graph parsing.
    Input that is ignored without error.

--- changelog.md
+++ changelog.md
@@ -1,12 +1,26 @@
 # Changelog
 
-**./src/parse-graph.js**
-* Wrote new function 'validateGraphParse'
-	* Checks whether the parsed graph has multiple nodes and edges.
-	* Called from 'performInputParsing'
-* Replaced `parseRes.valid = true;` with call to 'validateGraphParse'
-
----
-
-**./src/common/error-messages.js**
-* Wrote new function 'displayInvalidGraphErrorText'
+**./test-parts/test-graph-parse.js**
+* Declared global variable 'alphabet' - String from A to Z.
+* Declared global variable 'maxEdges'
+	* When testing the hard edge limit, a graph will be generated containing `maxEdges \* 1.15` edges, rounding up.
+	* Used as-is when checking final edge count.
+* Wrote unit tests for ignored data with the function 'handleIgnoreData'
+	* Whitespace
+	* Case Sensitivity
+	* All Possible Nodes
+	* Hard Edge Limit
+	* Duplicate Edges
+	* Recursive Edge
+	* Zero Distance
+* New secondary functions:
+	* defineWhitespaceEdges
+	* defineCaseSensitivityEdges
+	* defineDupeEdges
+	* defineRecurseEdges
+	* defineZeroDistEdges
+	* defineAlphabetGraph
+	* defineMaxEdgesGraph
+* Changes to 'checkParseResult'
+	* Split the `deep.equal` checks into their own function 'checkGraphContents'
+	* Removed parameters 'nodeArr' and 'edgeArr'
--- test-parts/test-graph-parse.js
+++ test-parts/test-graph-parse.js
@@ -3,12 +3,16 @@ const chai = require("chai");
 const expect = chai.expect;
 const parseGraph = require("../src/parse-graph");
 
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const maxEdges = 300;
+
 
 function runTests()
 {
 	describe("Parse Graph", function()
 	{
 		handleValidCase();
+		handleIgnoreData();
 	});
 }
 
@@ -23,11 +27,94 @@ function handleValidCase()
 			var inclEdges = defineValidCaseEdges();
 			var resultValue = parseGraph.performParsing("ME7, OW8, HI9, SH10");
 			
-			checkParseResult(resultValue, inclNodes, inclEdges);
+			checkParseResult(resultValue);
+			checkGraphContents(resultValue, inclNodes, inclEdges);
 		});
 		
 	});
 }
+
+
+function handleIgnoreData()
+{
+	describe("Ignore Data", function()
+	{
+		it("Whitespace", function()
+		{
+			var inpGraph = "AB3, CD6,EF5,         GH8, IJ10";
+			var inclNodes = "ABCDEFGHIJ".split("");
+			var inclEdges = defineWhitespaceEdges();
+			var resultValue = parseGraph.performParsing(inpGraph);
+			
+			checkParseResult(resultValue);
+			checkGraphContents(resultValue, inclNodes, inclEdges);
+		});
+		
+		
+		it("Case Sensitivity", function()
+		{
+			var inclNodes = "ABCD".split("");
+			var inclEdges = defineCaseSensitivityEdges();
+			var resultValue = parseGraph.performParsing("Ab8, cD16");
+			
+			checkParseResult(resultValue);
+			checkGraphContents(resultValue, inclNodes, inclEdges);
+		});
+		
+		it("All Possible Nodes", function()
+		{
+			var inpGraph = defineAlphabetGraph();
+			var inclNodes = alphabet.split("");
+			var resultValue = parseGraph.performParsing(inpGraph);
+			
+			checkParseResult(resultValue);
+			expect(resultValue.nodes).to.deep.equal(inclNodes);
+			expect(resultValue.edges.length).to.equal(26);
+		});
+		
+		it("Hard Edge Limit", function()
+		{
+			var inpGraph = defineMaxEdgesGraph();
+			var resultValue = parseGraph.performParsing(inpGraph);
+			
+			checkParseResult(resultValue);
+			expect(resultValue.nodes.length).to.be.at.least(2);
+			expect(resultValue.edges.length).to.equal(maxEdges);
+		});
+		
+		it("Duplicate Edges", function()
+		{
+			var inclNodes = "ABCD".split("");
+			var inclEdges = defineDupeEdges();
+			var resultValue = parseGraph.performParsing("AB5, CD7, AB9");
+			
+			checkParseResult(resultValue);
+			checkGraphContents(resultValue, inclNodes, inclEdges);
+		});
+		
+		it("Recursive Edge", function()
+		{
+			var inclNodes = "ABCDE".split("");
+			var inclEdges = defineRecurseEdges();
+			var resultValue = parseGraph.performParsing("AB6, CC10, DE12");
+			
+			checkParseResult(resultValue);
+			checkGraphContents(resultValue, inclNodes, inclEdges);
+		});
+		
+		it("Zero Distance", function()
+		{
+			var inclNodes = "ABEF".split("");
+			var inclEdges = defineZeroDistEdges();
+			var resultValue = parseGraph.performParsing("AB8, CD0, EF16");
+			
+			checkParseResult(resultValue);
+			checkGraphContents(resultValue, inclNodes, inclEdges);
+		});
+		
+	});
+}
+
 
 
 function defineValidCaseEdges()
@@ -44,7 +131,124 @@ function defineValidCaseEdges()
 }
 
 
-function checkParseResult(parseObj, nodeArr, edgeArr)
+function defineWhitespaceEdges()
+{
+	var edgeRes =
+	[
+		{"origin": 0, "destination": 1, "distance": 3},
+		{"origin": 2, "destination": 3, "distance": 6},
+		{"origin": 4, "destination": 5, "distance": 5},
+		{"origin": 6, "destination": 7, "distance": 8},
+		{"origin": 8, "destination": 9, "distance": 10}
+	];
+	
+	return edgeRes;
+}
+
+
+function defineCaseSensitivityEdges()
+{
+	var edgeRes =
+	[
+		{"origin": 0, "destination": 1, "distance": 8},
+		{"origin": 2, "destination": 3, "distance": 16},
+	];
+	
+	return edgeRes;
+}
+
+
+function defineDupeEdges()
+{
+	var edgeRes =
+	[
+		{"origin": 0, "destination": 1, "distance": 5},
+		{"origin": 2, "destination": 3, "distance": 7},
+	];
+	
+	return edgeRes;
+}
+
+
+function defineRecurseEdges()
+{
+	var edgeRes =
+	[
+		{"origin": 0, "destination": 1, "distance": 6},
+		{"origin": 3, "destination": 4, "distance": 12},
+	];
+	
+	return edgeRes;
+}
+
+
+function defineZeroDistEdges()
+{
+	var edgeRes =
+	[
+		{"origin": 0, "destination": 1, "distance": 8},
+		{"origin": 2, "destination": 3, "distance": 16},
+	];
+	
+	return edgeRes;
+}
+
+
+
+function defineAlphabetGraph()
+{
+	var stringRes = "";
+	
+	stringRes += "AB1, CD2, EF3, GH4, IJ5, KL6, MN7, OP8, QR9, ST10, UV11, WX12, YZ13, ";
+	stringRes += "ZY14, XW15, VU16, TS17, RQ18, PO19, NM20, LK21, JI22, HG23, FE24, DC25, BA26";
+	
+	return stringRes;
+}
+
+
+function defineMaxEdgesGraph()
+{
+	var originIndex = -1;
+	var destinationIndex = -1;
+	var distanceNumber = 10;
+	
+	var currentOriginNode = "";
+	var currentDestinationNode = "";
+	var currentPart = "";
+	
+	var partArray = [];
+	var loopCutoff = Math.ceil(maxEdges * 1.15);
+	var stringRes = "";
+	
+	while (partArray.length < loopCutoff && originIndex < alphabet.length)
+	{
+		if (destinationIndex <= 0)
+		{
+			originIndex += 1;
+			destinationIndex = 0;
+		}
+		
+		if (originIndex !== destinationIndex)
+		{
+			currentOriginNode = alphabet.charAt(originIndex);
+			currentDestinationNode = alphabet.charAt(destinationIndex);
+			currentPart = [currentOriginNode, currentDestinationNode, distanceNumber].join("");
+			partArray.push(currentPart);
+			
+			distanceNumber += 1;
+		}
+		
+		destinationIndex += 1;
+		destinationIndex = (destinationIndex % alphabet.length);
+	}
+	
+	stringRes = partArray.join();
+	return stringRes;
+}
+
+
+
+function checkParseResult(parseObj)
 {
 	expect(parseObj).to.not.be.undefined;
 	expect(parseObj).to.not.be.null;
@@ -57,11 +261,14 @@ function checkParseResult(parseObj, nodeArr, edgeArr)
 	expect(parseObj.nodes).to.be.an("array").that.is.not.empty;
 	expect(parseObj.edges).to.be.an("array").that.is.not.empty;
 	expect(parseObj.valid).to.be.true;
-	
+}
+
+
+function checkGraphContents(parseObj, nodeArr, edgeArr)
+{
 	expect(parseObj.nodes).to.deep.equal(nodeArr);
 	expect(parseObj.edges).to.deep.equal(edgeArr);
 }
-
 
 
 module.exports = runTests;
