e0009106dce459d827629d42a4d48d91b2982db7
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-10-26 15:31:30 +1000

    Wrote unit testing for main submission

--- changelog.md
+++ changelog.md
@@ -1,31 +1,13 @@
 # Changelog
 
-**./cases.js**
-* New file
-	* Test cases will be performed here rather than in the main file.
-	* Equally accessible between main submission and unit tests.
-	* Returns both expected and actual output.
-
----
-
-**./submission.js**
-* Added new requirement: './cases'
+**./test-parts/test-submission.js**
+* New requirement: '../cases'
 * Removed requirements:
 	* exactRoute
 	* shortestRoute
 	* possibleRoutes
-	* routeCriteria
-	* numSigns
-* Removed globals:
-	* routeStopCriteria
-	* routeDistCriteria
-* Wrote new function 'callTestCases'. Merges the following:
-	* callExactRouteTestCases
-	* callPossibleRouteByStopCountTestCases
-	* callShortestRouteTestCases
-	* callPossibleRouteByDistanceTestCase
-
----
-
-**./src/result-display.js**
-* Revised to use new test case structure.
+* Wrote unit testing functions:
+	* handlePreparation
+	* handleExampleCases
+	* handleDispose
+* Removed placeholder test.
--- test-parts/test-submission.js
+++ test-parts/test-submission.js
@@ -3,9 +3,7 @@ const chai = require("chai");
 const expect = chai.expect;
 const inputFile = require("../src/input-file");
 const parseGraph = require("../src/parse-graph");
-const exactRoute = require("../src/exact-route");
-const shortestRoute = require("../src/shortest-route");
-const possibleRoutes = require("../src/possible-routes");
+const cases = require("../cases");
 
 var inputGraphObject = null;
 
@@ -14,11 +12,130 @@ function runTests()
 {
 	describe("Submission", function()
 	{
-		it("Placeholder", function()
+		handlePreparation();
+		handleExampleCases();
+		handleDispose();
+	});
+}
+
+
+function handlePreparation()
+{
+	describe("Preparation", function()
+	{
+		var targetInputPath = "input.txt";
+		var rawText = null;
+		
+		it("Read Input File", function(done)
 		{
-			expect(true).to.be.true;
+			var inpEntry = inputFile.getEntry(targetInputPath);
+			var inpValid = inputFile.validateEntry(inpEntry);
+			rawText = inputFile.readContents(targetInputPath);
+			
+			done();
+		});
+		
+		it("Parse Graph", function(done)
+		{
+			inputGraphObject = parseGraph.performParsing(rawText);
+			rawText = null;
+			done();
+		});
+		
+	});
+}
+
+
+function handleExampleCases()
+{
+	describe("Example Cases", function()
+	{
+		it("Case 1", function()
+		{
+			var resultValue = cases.runCase1(inputGraphObject);
+			checkResultValue(resultValue);
+		});
+		
+		it("Case 2", function()
+		{
+			var resultValue = cases.runCase2(inputGraphObject);
+			checkResultValue(resultValue);
+		});
+		
+		it("Case 3", function()
+		{
+			var resultValue = cases.runCase3(inputGraphObject);
+			checkResultValue(resultValue);
+		});
+		
+		it("Case 4", function()
+		{
+			var resultValue = cases.runCase4(inputGraphObject);
+			checkResultValue(resultValue);
+		});
+		
+		it("Case 5", function()
+		{
+			var resultValue = cases.runCase5(inputGraphObject);
+			checkResultValue(resultValue);
+		});
+		
+		it("Case 6", function()
+		{
+			var resultValue = cases.runCase6(inputGraphObject);
+			checkResultValue(resultValue);
+		});
+		
+		it("Case 7", function()
+		{
+			var resultValue = cases.runCase7(inputGraphObject);
+			checkResultValue(resultValue);
+		});
+		
+		it("Case 8", function()
+		{
+			var resultValue = cases.runCase8(inputGraphObject);
+			checkResultValue(resultValue);
+		});
+		
+		it("Case 9", function()
+		{
+			var resultValue = cases.runCase9(inputGraphObject);
+			checkResultValue(resultValue);
+		});
+		
+		it("Case 10", function()
+		{
+			var resultValue = cases.runCase10(inputGraphObject);
+			checkResultValue(resultValue);
 		});
 	});
+}
+
+
+function handleDispose()
+{
+	describe("Dispose Input Graph", function()
+	{
+		it("Successful", function(done)
+		{
+			inputGraphObject = null;
+			done();
+		});
+	});
+}
+
+
+function checkResultValue(rVal)
+{
+	expect(rVal).to.not.be.undefined;
+	expect(rVal).to.not.be.null;
+	expect(rVal).to.be.an("object");
+	
+	expect(rVal).to.have.property("expected");
+	expect(rVal).to.have.property("actual");
+	
+	expect(rVal.actual).to.equal(rVal.expected);
 }
 
 
