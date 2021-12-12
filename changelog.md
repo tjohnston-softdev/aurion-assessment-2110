5b870d650d3bb24b31b5a48d9e7e2dd2009fa527
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-12-12 12:51:25 +1000

    Commented submission result display

--- changelog.md
+++ changelog.md
@@ -1,19 +1,4 @@
 # Changelog
 
 **./src/result-display.js**
-* Comment is now header for the entire file and not just 'outputResultsToConsole'
-* Declared global variables indicating column start points:
-	* caseCol
-	* expectCol
-	* actualCol
-* Wrote new function 'renderHeadRow'
-	* Used to write header row and output to console.
-	* Columns for case number, expected value, and actual value.
-* Wrote new function 'renderBodyRow'
-	* Used to write body row and output to console.
-	* Test case results.
-* Changes to 'outputResultsToConsole'
-	* 'renderHeadRow' is called before the loop.
-	* Removed 'currentValue' and 'currentLine' variables.
-	* Removed `console.log(currentLine)`
-	* Added call to 'renderBodyRow' during the loop.
+* Added comments to file.
--- src/result-display.js
+++ src/result-display.js
@@ -1,21 +1,27 @@
 // Output test results to console.
 
+
+// Table column start points.
 const caseCol = 2;
 const expectCol = 17;
 const actualCol = 42;
 
 
+// Main function.
 function outputResultsToConsole(resultArray)
 {
 	var caseIndex = 0;
 	var currentNumber = -1;
 	var currentCase = {};
 	
+	// Display table header.
 	console.log("");
 	renderHeadRow();
 	
+	// Loop test cases.
 	for (caseIndex = 0; caseIndex < resultArray.length; caseIndex = caseIndex + 1)
 	{
+		// Display row for current case.
 		currentNumber = caseIndex + 1;
 		currentCase = resultArray[caseIndex];
 		renderBodyRow(currentNumber, currentCase);
@@ -23,10 +29,12 @@ function outputResultsToConsole(resultArray)
 }
 
 
+// Displays table header row.
 function renderHeadRow()
 {
 	var rowText = "";
 	
+	// Loop line characters until all columns rendered.
 	while (rowText.length <= actualCol)
 	{
 		if (rowText.length === caseCol)
@@ -43,6 +51,7 @@ function renderHeadRow()
 		}
 		else
 		{
+			// Blank space.
 			rowText += " ";
 		}
 	}
@@ -51,11 +60,13 @@ function renderHeadRow()
 }
 
 
+// Displays body row for given test case.
 function renderBodyRow(caseNum, caseObj)
 {
 	var currentSubstring = "";
 	var rowText = "";
 	
+	// Loop line characters until all columns rendered.
 	while (rowText.length <= actualCol)
 	{
 		currentSubstring = " ";
