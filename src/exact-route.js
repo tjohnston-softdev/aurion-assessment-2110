6776ed7c8b294e3c8f69ace14c56a6524072b5ae
tjohnston-softdev <78240416+tjohnston-softdev@users.noreply.github.com>
2022-11-11 17:30:21 +1000

    Updated coding style for exact route

--- src/exact-route.js
+++ src/exact-route.js
@@ -8,19 +8,16 @@ function getRouteDistance(inputGraphObject, pathString)
 	var argValid = (typeof pathString === "string");
 	var resultValue = null;
 	
-	if (argValid === true && pathString.length >= 2)
+	if (argValid && pathString.length >= 2)
 	{
-		// Follow path.
 		resultValue = loopSteps(inputGraphObject, pathString);
 	}
-	else if (argValid === true)
+	else if (argValid)
 	{
-		// Too short.
 		resultValue = "PATH MUST HAVE AT LEAST TWO NODES";
 	}
 	else
 	{
-		// Invalid type.
 		resultValue = "PATH MUST BE A VALID STRING";
 	}
 	
@@ -35,60 +32,31 @@ function loopSteps(graphObject, stepStr)
 	var offsetIndex = 1;
 	var loopCutoff = stepStr.length - 1;
 	
-	var currentOriginChar = "";
-	var currentDestChar = "";
-	var currentOriginID = -1;
-	var currentDestID = -1;
-	var currentEdge = null;
-	
 	var totalDistance = 0;
 	var canContinue = true;
-	var loopResult = null;
 	
 	// Loop route characters as: 0-1, 1-2, etc
-	while (loopIndex >= 0 && loopIndex < loopCutoff && canContinue === true)
+	while (loopIndex >= 0 && loopIndex < loopCutoff && canContinue)
 	{
 		// Read route nodes.
 		offsetIndex = loopIndex + 1;
-		currentOriginChar = stepStr.charAt(loopIndex);
-		currentDestChar = stepStr.charAt(offsetIndex);
-		currentOriginID = graphObject.nodes.indexOf(currentOriginChar);
-		currentDestID = graphObject.nodes.indexOf(currentDestChar);
+		var currentOriginChar = stepStr.charAt(loopIndex);
+		var currentDestChar = stepStr.charAt(offsetIndex);
+		var currentOriginID = graphObject.nodes.indexOf(currentOriginChar);
+		var currentDestID = graphObject.nodes.indexOf(currentDestChar);
 		
 		// Retrieve corresponding edge.
-		currentEdge = graphTasks.getEdge(currentOriginID, currentDestID, graphObject.edges);
+		var currentEdge = graphTasks.getEdge(currentOriginID, currentDestID, graphObject.edges);
 		
 		
-		if (currentEdge !== null)
-		{
-			// Valid step - Increment distance.
-			totalDistance += currentEdge.distance;
-		}
-		else
-		{
-			// Invalid step - Abort loop.
-			canContinue = false;
-		}
+		if (currentEdge !== null) totalDistance += currentEdge.distance;
+		else canContinue = false;
 		
 		// Continue onto next step.
-		loopIndex = loopIndex + 1;
+		loopIndex += 1;
 	}
 	
-	
-	// Check if route successful.
-	if (canContinue === true)
-	{
-		// Return total distance.
-		loopResult = totalDistance;
-	}
-	else
-	{
-		// Return message.
-		loopResult = graphTasks.getNoRouteText();
-	}
-	
-	
-	return loopResult;
+	return canContinue ? totalDistance : graphTasks.getNoRouteText();
 }
 
 
